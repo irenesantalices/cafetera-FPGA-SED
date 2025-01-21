385f9d73677712861233d8a65f76b848abef02d9
irenesantalices <100681321+irenesantalices@users.noreply.github.com>
2025-01-21 21:22:43 +0100

    Add files via upload

---
+++ Trabajo_cafetera/Trabajo_cafetera.srcs/sim_1/new/test_completo.vhd
@@ -0,0 +1,128 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity test_completo is
+--  Port ( );
+end test_completo;
+
+architecture Behavioral of test_completo is
+
+  constant T_CLK: time := 1 us;
+constant tiempo: natural := 6;
+  signal fin: boolean := false;
+    signal final_tiempo : std_logic:='0';
+    signal digctrl  : std_logic_vector(7 downto 0):="11111111"; --out
+    signal CLK      : std_logic; --in
+    signal sw_encendido : std_logic:='0'; --in
+    signal  RST : std_logic;
+--    led_fin_tiempo :out std_logic;
+   signal  leds : std_logic_vector(2 downto 0);
+   signal display : std_logic_vector(6 downto 0):="0000000";
+  signal      boton_confirmar     : std_logic;
+  signal      boton_corto         :   STD_LOGIC;    -- Señal del botón para café corto
+  signal      boton_largo         :   STD_LOGIC;    -- Señal del botón para café largo
+  signal       boton_personalizar  :   STD_LOGIC;    -- Señal pdel botón para personalizar tiempo
+  signal      boton_no            :  STD_LOGIC;    -- Señal para decir que no (debe ser un boton)
+   signal     modo_cafe           :  STD_LOGIC_VECTOR(1 downto 0); 
+        -- Modo: 00 = apagado, 01 = preguntando, 10 = cafe solo y 11 = cafe con leche
+
+begin
+
+dut: entity work.top (Behavioral)
+     port map(digctrl => digctrl,
+        final_tiempo => final_tiempo,  
+        boton_confirmar => boton_confirmar, 
+  		boton_corto => boton_corto,
+        boton_largo => boton_largo,      
+        boton_personalizar => boton_personalizar,
+        boton_no => boton_no,         
+        sw_encendido => sw_encendido,  
+        clk => clk,                 
+        RST => RST,   
+        leds => leds,
+        display => display         
+     );
+
+  process
+  begin
+    clk <= '0';
+    wait for T_CLK/2;
+    clk <= '1';
+    if fin = true then
+      wait;
+    end if;
+    wait for T_CLK/2;
+
+  end process;
+
+  process
+  begin
+    RST <= '1';
+    boton_corto <= '0';
+    boton_largo <= '0';  
+    boton_personalizar <= '0';
+    boton_confirmar <= '0';
+    boton_no <= '0';
+    sw_encendido <= '0';     
+ 
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    
+    RST <= '0';
+    sw_encendido <= '1'; --estado cafe solo
+    boton_corto <= '1'; 
+
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    boton_no <='1';
+    boton_corto <= '0';
+
+    wait until clk'event and clk = '1';
+
+    sw_encendido <= '0';
+    
+    wait until clk'event and clk = '1';
+
+    sw_encendido <= '1';
+    
+    wait until clk'event and clk = '1';
+    
+    boton_personalizar <= '1';
+    
+    wait until clk'event and clk = '1';
+
+    boton_largo <= '1';
+    boton_personalizar <='0';
+    
+    wait until clk'event and clk = '1';
+    
+    boton_largo <= '0';
+    boton_confirmar <='1';
+    
+    wait until clk'event and clk = '1';
+
+    boton_confirmar <='0';
+    boton_personalizar <='0';
+    
+    wait until clk'event and clk = '1';
+    
+boton_confirmar <='1';
+    
+    wait until clk'event and clk = '1';
+
+    boton_confirmar <='0';
+    boton_corto <= '1';
+    
+    wait until clk'event and clk = '1';
+    
+    boton_corto <= '0';
+for i in 0 to 50 loop
+      wait until clk'event and clk = '1';
+    end loop;
+    fin <= true;
+    wait;
+  end process;
+
+end Behavioral;

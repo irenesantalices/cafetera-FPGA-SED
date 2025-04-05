f5d44ed24d035051986f764fbffa165e00ba7761
irenesantalices <100681321+irenesantalices@users.noreply.github.com>
2025-04-05 19:55:08 +0200

    Add files via upload

--- Trabajo_cafetera/Trabajo_cafetera.srcs/sim_1/new/tb_Temporizador.vhd
+++ Trabajo_cafetera/Trabajo_cafetera.srcs/sim_1/new/tb_Temporizador.vhd
@@ -8,9 +8,13 @@ end tb_Temporizador;
 architecture Behavioral of tb_Temporizador is
 
     -- Constants for testbench
-    constant CLK_PERIOD : time := 10 ns;
-
+ constant T_CLK: time := 1 us;
+constant tiempo: natural := 6;
+  signal fin: boolean := false;
     -- Signals to connect to the DUT
+    signal inicial : std_logic:='0';
+    signal unidades : std_logic_vector(3 downto 0);
+    signal decenas : std_logic_vector(3 downto 0);
     signal start : std_logic := '0';
     signal CLK : std_logic := '0';
     signal tiempo_in : std_logic_vector(5 downto 0) := (others => '0');
@@ -18,9 +22,9 @@ architecture Behavioral of tb_Temporizador is
     signal Pause : std_logic := '0';
     signal display1 : std_logic_vector(6 downto 0);
     signal display2 : std_logic_vector(6 downto 0);
-    signal segundos    : integer;
-    signal decenas     : integer;
     signal final_tiempo : std_logic;
+    signal code_dec :std_logic_vector(3 downto 0);
+    signal code_unit :std_logic_vector(3 downto 0);
 
     -- Clock generation process
    
@@ -28,12 +32,13 @@ architecture Behavioral of tb_Temporizador is
 begin
 
     -- Instantiate the DUT
-    DUT: entity work.Temporizador
-        generic map (
-            width => 6,
-            tiempo => 6
-        )
+    dut: entity work.Temporizador(Behavioral)
+--        generic map (
+--            width => 6,
+--            tiempo => 6
+--        )
         port map (
+            inicial => inicial,
             start => start,
             CLK => CLK,
             tiempo_in => tiempo_in,
@@ -41,49 +46,110 @@ begin
             Pause => Pause,
             display1 => display1,
             display2 => display2,
-            segundos =>segundos,
+            code_dec => code_dec,
+            code_unit => code_unit,
+            unidades => unidades,
             decenas => decenas,
             final_tiempo => final_tiempo
         );
 
     -- Test process
      process
-    begin
-        while true loop
-            CLK <= '0';
-            wait for CLK_PERIOD / 2;
-            CLK <= '1';
-            wait for CLK_PERIOD / 2;
-        end loop;
-    end process;
-    process
-    begin
-        -- Test Case 1: Initialize and wait
-        report "Starting Test Case 1: Initialize and wait.";
-        start <= '0';
-        tiempo_in <= "001010"; -- 10 seconds
-        Habilitar_T <= "00";
-        Pause <= '0';
-        wait for 100 ns;
+  begin
+    clk <= '0';
+    wait for T_CLK/2;
+    clk <= '1';
+    if fin = true then
+      wait;
+    end if;
+    wait for T_CLK/2;
 
-        -- Test Case 2: Enable and start counting
-        report "Starting Test Case 2: Enable and start counting.";
-        Habilitar_T <= "11";
-        start <= '1';
-        wait for 3 ns; -- Simulate some time
+  end process;
 
-        -- Test Case 3: Pause the timer
-        report "Starting Test Case 3: Pause the timer.";
-        Pause <= '1';
-        wait for 10 ns;
-        Pause <= '0';
+        process
+  begin
+    start <= '0';
+    tiempo_in <= "000000";
+    Habilitar_T <= "00";
+    Pause <= '0';
+--    display1 <= "0000000";    
+--    display2 <= "0000000";    
 
-        -- Test Case 4: Countdown complete
-        report "Starting Test Case 4: Countdown complete.";
-        wait for 10 ns; -- Wait for countdown to finish
-        Habilitar_T <= "00";
-        report "Testbench completed.";
-        wait;
-    end process;
+ 
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    
+    start <= '1';
+    Habilitar_T <="10";
+    wait until clk'event and clk = '1';
+     
+    tiempo_in <= "000010";
+    
+    wait until clk'event and clk = '1';
+    start <= '0';
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+
+    start <= '0';
+
+    wait until clk'event and clk = '1';
+
+    tiempo_in <= "000100";
+    
+    wait until clk'event and clk = '1';
+
+    start <= '1';
+    
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+
+
+    start <= '0';
+    for i in 0 to 3 loop
+      wait until clk'event and clk = '1';
+    end loop;
+    
+    Pause <= '1';
+    wait until clk'event and clk = '1';
+
+    Pause <= '0'; --estado apagada
+
+    
+  for i in 0 to 5 loop
+      wait until clk'event and clk = '1';
+    end loop;
+    
+    start <='0';
+    Habilitar_T <= "00";
+    
+    tiempo_in <="010000";
+    
+    wait until clk'event and clk = '1';
+    
+    start <= '1';
+
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+
+    Habilitar_T <="11";
+    
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    
+    start <='0';
+    for i in 0 to 5 loop
+      wait until clk'event and clk = '1';
+    end loop;
+    
+     for i in 0 to 10 loop
+      wait until clk'event and clk = '1';
+    end loop;
+    wait until clk'event and clk = '1';
+    fin <= true;
+    wait;
+  end process;
+
 
 end Behavioral;

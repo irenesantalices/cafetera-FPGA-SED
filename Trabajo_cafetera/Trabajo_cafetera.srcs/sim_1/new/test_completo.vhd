43a171b5c01e9104eaa8f768f370a78008745802
irenesantalices <100681321+irenesantalices@users.noreply.github.com>
2025-06-12 19:12:44 +0200

    Add files via upload

--- Trabajo_cafetera/Trabajo_cafetera.srcs/sim_1/new/test_completo.vhd
+++ Trabajo_cafetera/Trabajo_cafetera.srcs/sim_1/new/test_completo.vhd
@@ -12,11 +12,16 @@ architecture Behavioral of test_completo is
     constant T_CLK: time := 1 us;
     constant tiempo: natural := 6;
     signal fin: boolean := false;
-    --signal final_tiempo : std_logic:='0';
+    signal final_tiempo : std_logic:='0';
     signal digctrl  : std_logic_vector(7 downto 0):="11111111"; --out
     signal CLK      : std_logic; --in
     signal sw_encendido : std_logic:='0'; --in
+    signal tiempo_selec: std_logic;
     signal  RST : std_logic;
+    signal personalizar :  std_logic;
+    signal start_tiempo:  std_logic;
+    signal contando : std_logic;
+
     --    led_fin_tiempo :out std_logic;
     signal  leds : std_logic_vector(3 downto 0);
     signal display : std_logic_vector(6 downto 0):="0000000";
@@ -26,13 +31,17 @@ architecture Behavioral of test_completo is
     signal       boton_personalizar  :   STD_LOGIC;    -- Señal pdel botón para personalizar tiempo
     signal      boton_no            :  STD_LOGIC;    -- Señal para decir que no (debe ser un boton)
     signal     modo_cafe           :  STD_LOGIC_VECTOR(1 downto 0);
+    signal        tiempo_out        :  STD_LOGIC_VECTOR(tiempo-1 downto 0);
+
     -- Modo: 00 = apagado, 01 = preguntando, 10 = cafe solo y 11 = cafe con leche
 
 begin
 
     dut: entity work.top (Behavioral)
         port map(digctrl => digctrl,
-                 --final_tiempo => final_tiempo,  
+                 final_tiempo => final_tiempo,  
+                 start_tiempo => start_tiempo,
+                 tiempo_selec=>tiempo_selec,
                  boton_confirmar => boton_confirmar,
                  boton_corto => boton_corto,
                  boton_largo => boton_largo,
@@ -42,6 +51,9 @@ begin
                  clk => clk,
                  RST => RST,
                  leds => leds,
+                 contando =>contando,
+                 tiempo_out=> tiempo_out,
+                 out_personalizar =>personalizar,
                  display => display
                 );
 
@@ -70,19 +82,19 @@ begin
         wait until clk'event and clk = '1';
         wait until clk'event and clk = '1';
 
-        RST <= '0';
+        RST <= '1'; --boton reset negado
         sw_encendido <= '1'; --estado cafe solo
 
         wait until clk'event and clk = '1';
         wait until clk'event and clk = '1';
 
 
-        boton_corto <= '1';
+        boton_largo <= '1';
 
         wait until clk'event and clk = '1';
         wait until clk'event and clk = '1';
-        boton_corto <= '0';
-        for i in 0 to 11 loop
+        boton_largo <= '0';
+        for i in 0 to 20 loop
             wait until clk'event and clk = '1';
         end loop;
         boton_no <='1';
@@ -98,15 +110,36 @@ begin
         wait until clk'event and clk = '1';
 
         sw_encendido <= '1';
+        wait until clk'event and clk = '1';
+
+        wait until clk'event and clk = '1';
+         boton_largo <= '1';
 
         wait until clk'event and clk = '1';
         wait until clk'event and clk = '1';
+        boton_largo <= '0';
+        for i in 0 to 30 loop
+            wait until clk'event and clk = '1';
+        end loop;
+        wait until clk'event and clk = '1';
+        boton_no <= '1';
+        wait until clk'event and clk = '1';
+        boton_no <= '0';
+
         boton_personalizar <= '1';
 
         wait until clk'event and clk = '1';
 
         boton_largo <= '1';
         boton_personalizar <='0';
+         for i in 0 to 2 loop
+             wait until clk'event and clk = '1';
+              boton_largo <= '1';
+
+              wait until clk'event and clk = '1';
+
+         boton_largo <= '0';
+      end loop;
 
         wait until clk'event and clk = '1';
 
